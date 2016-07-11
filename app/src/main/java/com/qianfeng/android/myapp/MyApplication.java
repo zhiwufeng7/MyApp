1d5d5e2bb0d4674d4493e4799e84c26857e0a481
LiuXiaoDD <342879680@qq.com>
2016-07-11 22:23:43 +0800

    初始化

---
+++ app/src/main/java/com/qianfeng/android/myapp/MyApplication.java
@@ -0,0 +1,9 @@
+package com.qianfeng.android.myapp;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/7/11.
+ */
+public class MyApplication extends Application {
+}
---
+++ app/src/main/java/com/qianfeng/android/myapp/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.qianfeng.android.myapp.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.beijing.android.mymovie.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
