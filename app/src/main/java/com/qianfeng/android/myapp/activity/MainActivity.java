d5b65872b32a06db5a96c976e9a0a035cbe7065e
liuzhao93 <347755658@qq.com>
2016-07-13 00:33:43 +0800

    07/13 主界面内容

--- app/src/main/java/com/qianfeng/android/myapp/activity/MainActivity.java
+++ app/src/main/java/com/qianfeng/android/myapp/activity/MainActivity.java
@@ -3,7 +3,9 @@ package com.qianfeng.android.myapp.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.RadioGroup;
 
+import com.handmark.pulltorefresh.library.PullToRefreshExpandableListView;
 import com.qianfeng.android.myapp.R;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
@@ -11,26 +13,38 @@ import com.zhy.http.okhttp.callback.StringCallback;
 import okhttp3.Call;
 
 public class MainActivity extends AppCompatActivity {
+    private PullToRefreshExpandableListView refreshListView;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        OkHttpUtils.postString()
-                .url("http://mobileif.maizuo.com/ver4/banner/list/v2?cityId=15&version=4.7&agentId=wandoujia&channelID=31&clientID=31&revision=4.7&agentID=wandoujia")
-                .content("{'cityId':'15','type':'2'}")
-                .build()
-                .execute(new StringCallback() {
-                    @Override
-                    public void onError(Call call, Exception e, int id) {
+        initView();
 
-                    }
 
-                    @Override
-                    public void onResponse(String response, int id) {
-                        Log.e("hehe",response);
-                    }
-                });
+//        OkHttpUtils.postString()
+//                .url("http://mobileif.maizuo.com/ver4/banner/list/v2?cityId=15&version=4.7&agentId=wandoujia&channelID=31&clientID=31&revision=4.7&agentID=wandoujia")
+//                .content("{'cityId':'15','type':'2'}")
+//                .build()
+//                .execute(new StringCallback() {
+//                    @Override
+//                    public void onError(Call call, Exception e, int id) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onResponse(String response, int id) {
+//                        Log.e("hehe",response);
+//                    }
+//                });
+    }
+
+    private void initView() {
+
+        refreshListView = (PullToRefreshExpandableListView) findViewById(R.id.main_content_ev);
+        radioGroup = (RadioGroup) findViewById(R.id.main_bottom_rg);
+
     }
 }
---
+++ app/src/main/java/com/qianfeng/android/myapp/bean/HomePageEV.java
@@ -0,0 +1,10 @@
+package com.qianfeng.android.myapp.bean;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/7/13.
+ */
+public class HomePageEV {
+
+}
