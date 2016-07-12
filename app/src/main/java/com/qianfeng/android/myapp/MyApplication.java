945835f7c9f29cc1b30bbfc6ff987591eaf9bab4
my <347755658@qq.com>
2016-07-12 14:16:43 +0800

    注释

--- app/src/main/java/com/qianfeng/android/myapp/MyApplication.java
+++ app/src/main/java/com/qianfeng/android/myapp/MyApplication.java
@@ -6,4 +6,5 @@ import android.app.Application;
  * Created by Administrator on 2016/7/11.
  */
 public class MyApplication extends Application {
+
 }
--- app/src/main/java/com/qianfeng/android/myapp/activity/MainActivity.java
+++ app/src/main/java/com/qianfeng/android/myapp/activity/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //123
+        //1111
         finish();
     }
 }
