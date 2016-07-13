b4e3ecc11e1ac822b62c05b406e0152413262bec
LiuXiaoDD <342879680@qq.com>
2016-07-13 12:14:19 +0800

    加载百度定位数据

--- app/src/main/java/com/qianfeng/android/myapp/activity/MainActivity.java
+++ app/src/main/java/com/qianfeng/android/myapp/activity/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.qianfeng.android.myapp.MyApplication;
 import com.qianfeng.android.myapp.R;
 import com.qianfeng.android.myapp.fragment.HomePageFragment;
 
@@ -20,45 +21,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         initView();
         initFragment();
-
         initListener();
-
-
+        ((MyApplication)getApplication()).mLocationClient.start();
     }
 
     private void initListener() {
 
-
     }
 
     private void initFragment() {
-
-        FragmentManager manager=getSupportFragmentManager();
-        FragmentTransaction transition=manager.beginTransaction();
-        HomePageFragment homePageFragment=new HomePageFragment();
-        transition.add(R.id.main_content_fl,homePageFragment);
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transition = manager.beginTransaction();
+        HomePageFragment homePageFragment = new HomePageFragment();
+        transition.add(R.id.main_content_fl, homePageFragment);
         transition.commit();
 
     }
 
     private void initView() {
-
         radioGroup = (RadioGroup) findViewById(R.id.main_bottom_rg);
         setUpRadioGroup();
     }
 
     private void setUpRadioGroup() {
-
         int size = radioGroup.getChildCount();
         RadioButton[] radioButtons = new RadioButton[size];
-
         for (int i = 0; i < size; i++) {
             radioButtons[i] = (RadioButton) radioGroup.getChildAt(i);
         }
-
         radioButtons[0].setChecked(true);
     }
 
